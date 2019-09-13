e3d47183186eb9e00d6cfdff8a050b62e15b323f
Fernando <mymail.tsts.edu>
2019-09-13 13:58:52 +0000

    Worked on ex.

--- CC1/CC1-Ex24/code.py
+++ CC1/CC1-Ex24/code.py
@@ -1,1 +1,3 @@
 zen_of_python = 'Namespaces are one freaking great idea -- lets do more of those!'
+zen_of_python_2 = zen_of_python.replace('freaking' , 'honking')
+print(zen_of_python_2)
--- CC1/CC1-Ex31/code.py
+++ CC1/CC1-Ex31/code.py
@@ -1,1 +1,3 @@
 str = 'X-DSPAM-Confidence:0.8475'
+type(str)
+print(float(str[19:]))
